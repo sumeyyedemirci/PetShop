568f97e58805afed9d7d9014c4f8e79cedc2df4a
sumeyyedemirci <84576826+sumeyyedemirci@users.noreply.github.com>
2021-06-17 18:17:05 +0300

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1000, function(){
+          window.location.hash = hash;
+        });
+      } 
+    });
+  });
+
+  //scroll top js
+  var btn = $('#button');
+
+      $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+          btn.addClass('show');
+        } else {
+          btn.removeClass('show');
+        }
+      });
+
+      btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '700');
+      });
